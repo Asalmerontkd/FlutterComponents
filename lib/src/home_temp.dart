87db32da470f7e2b542e9b24f12a65abdaee19fe
Asalmerontkd <psrantonio@gmail.com>
2020-05-14 16:57:58 -0500

    List y ListTile

--- lib/src/home_temp.dart
+++ lib/src/home_temp.dart
@@ -1,11 +1,45 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
+  final opciones = ['uno', 'dos', 'tres', 'cuatro', 'cinco'];
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home temp'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes TEMP'),
+      ),
+      body: ListView(children: _crearItemsCorto()),
     );
+  }
+
+  List<Widget> _crearItems() {
+    List<Widget> lista = new List<Widget>();
+
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+      lista..add(tempWidget)..add(Divider());
+    }
+
+    return lista;
+  }
+
+  List<Widget> _crearItemsCorto() {
+    return opciones.map((item) { 
+      return Column(
+        children: <Widget>[
+          ListTile(
+            title: Text(item + '!'),
+            subtitle: Text('Mi subtitulo.'),
+            leading: Icon(Icons.language),
+            trailing: Icon(Icons.arrow_forward_ios),
+            onTap: () {},
+          ),
+          Divider(color: Color(Colors.blue.value),)
+        ],
+      );
+    }).toList();
   }
 }
