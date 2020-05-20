cdc1598d9540f6b544f82a1c57874b9423dc73cd
Asalmerontkd <psrantonio@gmail.com>
2020-05-19 20:31:56 -0500

    archivo JSON, Map, Icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
+import 'package:componentes/src/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
-import 'package:componentes/src/home_temp.dart';
- 
 void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
@@ -10,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Componentes APP.',
       debugShowCheckedModeBanner: false,
-      home: HomePageTemp()
+      home: HomePage()
     );
   }
 }
---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,57 @@
+import 'package:componentes/src/utils/icono_string_util.dart';
+import 'package:flutter/material.dart';
+import 'package:componentes/src/providers/menu_provider.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes'),
+      ),
+      body: _lista(),
+    );
+  }
+
+  Widget _lista() {
+    //menuProvider.opciones;
+    /* La siguiente llamada obtiene los valores del future 
+    pero no se recomienda debido a que la petición puede tardar
+    menuProvider.cargarData().then((opciones) {
+      print('_Lista');
+      print(opciones);
+    });
+
+    return ListView(
+      children: _listItems(),
+    );*/
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+        print('builder');
+        print(snapshot.data);
+
+        return ListView(
+          children: _listItems(snapshot.data),
+        );
+      },
+    );
+  }
+
+  List<Widget> _listItems( List<dynamic> data ) {
+    final List<Widget> opciones = [];
+    data.forEach((element) {
+      final widgetTmp = ListTile(
+        title: Text(element['texto']),
+        leading: getIcon(element['icon']),
+        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue,),
+        onTap: () {},
+      );
+
+      opciones..add(widgetTmp)
+              ..add(Divider());
+    });
+    return opciones;
+  }
+}
