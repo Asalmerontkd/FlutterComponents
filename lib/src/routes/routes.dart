56a5347d321424fa0d1e484bdaf00ff323684ff8
Asalmerontkd <psrantonio@gmail.com>
2020-05-28 22:53:39 -0500

    Card simple

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:componentes/src/pages/card_page.dart';
 import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
@@ -7,6 +8,7 @@ Map<String, WidgetBuilder> getApplicationRoutes(){
   return <String, WidgetBuilder>{
         'inicio' : (BuildContext context) => HomePage(),
         'alert' : (BuildContext context) => AlertPage(),
-        'avatar' : (BuildContext context) => AvatarPage()
+        'avatar' : (BuildContext context) => AvatarPage(),
+        'card' : (BuildContext context) => CardPage()
       };
 }
