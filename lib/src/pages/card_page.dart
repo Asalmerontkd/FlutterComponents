96d69e29a125bc57015b38d9fecccdc0eea3962d
Asalmerontkd <psrantonio@gmail.com>
2020-05-28 23:42:55 -0500

    FadeInImage

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -8,7 +8,9 @@ class CardPage extends StatelessWidget {
       body: ListView(
         padding: EdgeInsets.all(10.0),
         children: <Widget>[
-          _cardTipo1()
+          _cardTipo1(),
+          SizedBox(height: 30.0,),
+          _cardTipo2()
         ],
       ),
     );
@@ -32,6 +34,24 @@ class CardPage extends StatelessWidget {
           )
         ],
       ),
+    );
+  }
+
+  Widget _cardTipo2(){
+    return Column(
+      children: <Widget>[
+        FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'), 
+          image: NetworkImage('https://www.yourtrainingedge.com/wp-content/uploads/2019/05/background-calm-clouds-747964.jpg'),
+          fadeInDuration: Duration(milliseconds: 200),
+          height: 300.0,
+          fit: BoxFit.cover,),
+        //Image(image: NetworkImage('https://www.yourtrainingedge.com/wp-content/uploads/2019/05/background-calm-clouds-747964.jpg')),
+        Container(
+          padding: EdgeInsets.all(10.0),
+          child: Text('Mi texto..'),
+        )
+      ],
     );
   }
 }
