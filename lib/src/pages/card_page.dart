1ada9147b2b2a05f9e17fb2a4130d6ab626383d0
Asalmerontkd <psrantonio@gmail.com>
2020-06-18 20:07:11 -0500

    Efecto redondeado y sombra en CARDS

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -4,32 +4,76 @@ class CardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Cards'),),
+      appBar: AppBar(
+        title: Text('Cards'),
+      ),
       body: ListView(
         padding: EdgeInsets.all(10.0),
         children: <Widget>[
           _cardTipo1(),
-          SizedBox(height: 30.0,),
-          _cardTipo2()
+          SizedBox(
+            height: 30.0,
+          ),
+          _cardTipo2(),
+          SizedBox(
+            height: 30.0,
+          ),
+          _cardTipo1(),
+          SizedBox(
+            height: 30.0,
+          ),
+          _cardTipo2(),
+          SizedBox(
+            height: 30.0,
+          ),
+          _cardTipo1(),
+          SizedBox(
+            height: 30.0,
+          ),
+          _cardTipo2(),
+          SizedBox(
+            height: 30.0,
+          ),
+          _cardTipo1(),
+          SizedBox(
+            height: 30.0,
+          ),
+          _cardTipo2(),
+          SizedBox(
+            height: 30.0,
+          ),
+          _cardTipo1(),
+          SizedBox(
+            height: 30.0,
+          ),
+          _cardTipo2(),
+          SizedBox(
+            height: 30.0,
+          )
         ],
       ),
     );
   }
 
-  Widget _cardTipo1(){
+  Widget _cardTipo1() {
     return Card(
+      elevation: 10.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
       child: Column(
         children: <Widget>[
           ListTile(
-            leading: Icon(Icons.photo_album, color: Colors.blue,),
+            leading: Icon(
+              Icons.photo_album,
+              color: Colors.blue,
+            ),
             title: Text('Titulo de tarjeta'),
             subtitle: Text('Subtitulo de la tarjeta'),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
-              FlatButton(onPressed: (){}, child: Text('Cancelar')),
-              FlatButton(onPressed: (){}, child: Text('OK')),
+              FlatButton(onPressed: () {}, child: Text('Cancelar')),
+              FlatButton(onPressed: () {}, child: Text('OK')),
             ],
           )
         ],
@@ -37,21 +81,44 @@ class CardPage extends StatelessWidget {
     );
   }
 
-  Widget _cardTipo2(){
-    return Column(
-      children: <Widget>[
-        FadeInImage(
-          placeholder: AssetImage('assets/jar-loading.gif'), 
-          image: NetworkImage('https://www.yourtrainingedge.com/wp-content/uploads/2019/05/background-calm-clouds-747964.jpg'),
-          fadeInDuration: Duration(milliseconds: 200),
-          height: 300.0,
-          fit: BoxFit.cover,),
-        //Image(image: NetworkImage('https://www.yourtrainingedge.com/wp-content/uploads/2019/05/background-calm-clouds-747964.jpg')),
-        Container(
-          padding: EdgeInsets.all(10.0),
-          child: Text('Mi texto..'),
-        )
-      ],
+  Widget _cardTipo2() {
+    final card = Container(
+      child: Column(
+        children: <Widget>[
+          FadeInImage(
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            image: NetworkImage(
+                'https://www.yourtrainingedge.com/wp-content/uploads/2019/05/background-calm-clouds-747964.jpg'),
+            fadeInDuration: Duration(milliseconds: 200),
+            height: 300.0,
+            fit: BoxFit.cover,
+          ),
+          //Image(image: NetworkImage('https://www.yourtrainingedge.com/wp-content/uploads/2019/05/background-calm-clouds-747964.jpg')),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('Mi texto..'),
+          )
+        ],
+      ),
+    );
+
+    return Container(
+      decoration: BoxDecoration( 
+        borderRadius: BorderRadius.circular(20.0),
+        color: Colors.white,
+        boxShadow: <BoxShadow> [
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 10.0,
+            spreadRadius: 2.0,
+            offset: Offset(2.0, 10.0)
+          )
+        ]
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20.0),
+        child: card,
+      ),
     );
   }
 }
