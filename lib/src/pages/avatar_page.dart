f74e46568d99296434ec70aadf1a52001fedd8e6
Asalmerontkd <psrantonio@gmail.com>
2020-06-25 12:49:35 -0500

    AnimatedContainder Curves

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,6 +6,30 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page.'),
+        actions: <Widget>[
+
+          Container(
+            padding: EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://png.pngtree.com/element_our/png_detail/20181102/avatar-profile-logo-vector-emblem-illustration-modern-illustration-png_227485.jpg'),
+              radius: 20.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('PS'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage('https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/03/18/15/billgates.jpg'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
